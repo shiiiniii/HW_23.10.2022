77aff051989befe02e523ddf277bfc641f11a6ea
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-24 19:12:05 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите целое положительное число: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    {
+        Console.WriteLine($"{i} x {i} x {i} = {i*i*i}");
+    }
+    Console.WriteLine();
+}
